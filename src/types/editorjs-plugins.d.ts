2633b78c3c259ad0ae3934acfe669cf9835e6dd4
Jeff Bocala <6222411+jeffbocala@users.noreply.github.com>
2021-04-13 20:05:36 +0700

    refactor: remove unused types

--- src/types/editorjs-plugins.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@editorjs/header';
-declare module '@editorjs/list';
-declare module '@editorjs/inline-code';
-declare module '@editorjs/marker';
-declare module 'editorjs-table';
-declare module '@editorjs/image';
